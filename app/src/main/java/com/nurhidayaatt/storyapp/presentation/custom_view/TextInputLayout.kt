cd7d1acdb96465aeb3c8980766e207208b83c40d
Nur Hidayat <nur.hidayat454.nh@gmail.com>
2024-01-29 17:03:30 +0700

    update AGP and library

--- app/src/main/java/com/nurhidayaatt/storyapp/presentation/custom_view/TextInputLayout.kt
+++ app/src/main/java/com/nurhidayaatt/storyapp/presentation/custom_view/TextInputLayout.kt
@@ -17,7 +17,7 @@ class TextInputLayout: TextInputLayout {
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    override fun onDraw(canvas: Canvas?) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
         editText?.let { editText ->
